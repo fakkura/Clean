e2e801eb6b00c0c39f2323b0254703f476e0f36f
fakkura <fakkura@gmail.com>
2016-12-15 22:44:13 +0100

    first

---
+++ Clean/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clean
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                if (this.Background == Brushes.White)
+                    this.Background = Brushes.Black;
+                else
+                    this.Background = Brushes.White;
+            else if (e.Key == Key.Escape)
+                this.Close();
+        }
+    }
+}
